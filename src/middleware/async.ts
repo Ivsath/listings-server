60d05c6e7ebf497fa9d169bb68d7a8688a98f84b
Irvin Reyes <reyes.yerp@gmail.com>
2020-08-11 12:06:20 -0500

    Remove unwanted files

--- src/middleware/async.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
-
-const asyncHandler = (fn: RequestHandler) => (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): Promise<void> => Promise.resolve(fn(req, res, next)).catch(next);
-
-export default asyncHandler;
