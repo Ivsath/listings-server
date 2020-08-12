93513c50137a21539b1c05331413c69671fc1da7
Irvin Reyes <reyes.yerp@gmail.com>
2020-08-12 13:28:40 -0500

    Update config setup

--- src/config/dev.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  secrets: {
-    jwt: 'asdasdadsadadasdsd',
-  },
-  dbUrl:
-    'mongodb+srv://asdasddsa:asdfghjjk@graphql-ts.arp5a.mongodb.net/test?retryWrites=true&w=majority',
-};
