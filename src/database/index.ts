d5b36a5216fbb46a68ce72e08b86354bfabf850d
Irvin Reyes <reyes.yerp@gmail.com>
2020-08-12 23:27:45 -0500

    Add return type to the db collection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,21 +1,18 @@
 import { MongoClient } from 'mongodb';
 
 import config from '../config';
+import { Database } from '../lib/types';
 
-export const connectDatabase = async () => {
-  try {
-    const client = await MongoClient.connect(config.dbUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    const db = client.db('main');
+export const connectDatabase = async (): Promise<Database> => {
+  const client = await MongoClient.connect(config.dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  const db = client.db('main');
 
-    console.log('DB connected');
+  console.log('DB connected');
 
-    return {
-      listings: db.collection('test_listings'),
-    };
-  } catch (error) {
-    console.error(error);
-  }
+  return {
+    listings: db.collection('test_listings'),
+  };
 };
---
+++ src/lib/types.ts
@@ -0,0 +1,16 @@
+import { Collection, ObjectId } from 'mongodb';
+
+export interface Listing {
+  _id: ObjectId;
+  title: string;
+  image: string;
+  address: string;
+  price: number;
+  numOfGuests: number;
+  numOfBeds: number;
+  rating: number;
+}
+
+export interface Database {
+  listings: Collection<Listing>;
+}
