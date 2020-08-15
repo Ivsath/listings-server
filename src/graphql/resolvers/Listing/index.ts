c2ab38fc7fe3b7b350165c1096a972c71d5003e0
Irvin Reyes <reyes.yerp@gmail.com>
2020-08-14 19:02:20 -0500

    Modularize the resolvers

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,9 +1,9 @@
 import { IResolvers } from 'apollo-server-express';
 import { ObjectID } from 'mongodb';
 
-import { Database, Listing } from '../lib/types';
+import { Database, Listing } from '../../../lib/types';
 
-export const resolvers: IResolvers = {
+export const listingResolvers: IResolvers = {
   Query: {
     listings: async (
       _root: undefined,
---
+++ src/graphql/resolvers/index.ts
@@ -0,0 +1,5 @@
+import { merge } from 'lodash';
+
+import { listingResolvers } from './Listing';
+
+export const resolvers = merge(listingResolvers);
