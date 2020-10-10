ad31699884d15bf315529a1004e5100750c00a78
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-10 15:07:48 -0500

    Update the graphql type definitions

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -2,7 +2,7 @@ import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
   type Listing {
-    id: ID!
+    id: ID
     title: String!
     image: String!
     address: String!
@@ -14,10 +14,13 @@ export const typeDefs = gql`
   }
 
   type Query {
+    listing(id: ID!): Listing!
     listings: [Listing!]!
   }
 
   type Mutation {
+    createListing: Listing
+    updateListing(id: ID!): Listing!
     deleteListing(id: ID!): Listing!
   }
 `;
