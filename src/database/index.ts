6c1696fe467669af12ceee113734dd963359f4b6
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-10 15:02:53 -0500

    Update the db connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,19 +1,12 @@
-import { MongoClient } from 'mongodb';
+import { createConnection } from 'typeorm';
 
 import { Database } from '../lib/types';
-
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net?retryWrites=true&w=majority`;
+import { Listing } from './entity/ListingEntity';
 
 export const connectDatabase = async (): Promise<Database> => {
-  const client = await MongoClient.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-  const db = client.db('main');
-
-  console.log('DB connected');
+  const connection = await createConnection();
 
   return {
-    listings: db.collection('test_listings'),
+    listings: connection.getRepository(Listing),
   };
 };
