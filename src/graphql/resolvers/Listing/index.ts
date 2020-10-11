5be81d334b6cde7eb44fb6f722ba5d1ca862ee5f
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-10 20:28:54 -0500

    Add new resolvers

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,34 +1,78 @@
 import { IResolvers } from 'apollo-server-express';
-import { ObjectID } from 'mongodb';
+import crypto from 'crypto';
 
 import { Database, Listing } from '../../../lib/types';
 
 export const listingResolvers: IResolvers = {
   Query: {
+    listing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database },
+    ): Promise<Listing> => {
+      const listing = await db.listings.findOne({ id });
+
+      if (!listing) throw new Error(`Failed to find listing with id: ${id}`);
+
+      return listing;
+    },
     listings: async (
       _root: undefined,
       _args: Record<string, unknown>,
       { db }: { db: Database },
     ): Promise<Listing[]> => {
-      return await db.listings.find({}).toArray();
+      return await db.listings.find();
     },
   },
   Mutation: {
+    createListing: async (
+      _root: undefined,
+      _args: Record<string, unknown>,
+      { db }: { db: Database },
+    ): Promise<Listing> => {
+      const newListing: Listing = {
+        id: crypto.randomBytes(16).toString('hex'),
+        title:
+          '[NEW] Clean and fully furnished apartment. 5 min away from CN Tower',
+        image:
+          'https://res.cloudinary.com/tiny-house/image/upload/v1560641352/mock/Toronto/toronto-listing-1_exv0tf.jpg',
+        address: '3210 Scotchmere Dr W, Toronto, ON, CA',
+        price: 10000,
+        numOfGuests: 2,
+        numOfBeds: 1,
+        numOfBaths: 2,
+        rating: 5,
+      };
+
+      return await db.listings.create(newListing).save();
+    },
+    updateListing: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database },
+    ): Promise<Listing> => {
+      const listing = await db.listings.findOne({ id });
+
+      if (!listing) {
+        throw new Error(`Failed to find listing with id: ${id}`);
+      }
+
+      listing.title = '[UPDATED] This is my updated title!';
+
+      return await listing.save();
+    },
     deleteListing: async (
       _root: undefined,
       { id }: { id: string },
       { db }: { db: Database },
     ): Promise<Listing> => {
-      const deleteRes = await db.listings.findOneAndDelete({
-        _id: new ObjectID(id),
-      });
+      const listing = await db.listings.findOne({ id });
 
-      if (!deleteRes.value) throw new Error('Failed to delete listing');
+      if (!listing) {
+        throw new Error(`Failed to find listing with id: ${id}`);
+      }
 
-      return deleteRes.value;
+      return await listing.remove();
     },
-  },
-  Listing: {
-    id: (listing: Listing): string => listing._id.toString(),
   },
 };
